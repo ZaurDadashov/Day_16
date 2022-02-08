edc52e5f36e39f2829d098dd0eb67e0afec1dcb0
ZaurDadashov <zevs.diy@gmail.com>
2022-02-08 10:29:35 +0400

    Start

---
+++ Writing_and_reading_files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Writing_and_reading_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
